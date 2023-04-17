a1104f3cca3b650589d82073d18c0504d0ca535c
2530470001 <2530470001@qq.com>
2023-04-17 11:54:25 +0800

    app登录bug修改

--- shopping-app/shopping_portal/src/main/java/com/coca/shopping_portal/ShoppingPortalApplication.java
+++ shopping-app/shopping_portal/src/main/java/com/coca/shopping_portal/ShoppingPortalApplication.java
@@ -3,11 +3,13 @@ package com.coca.shopping_portal;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableDubbo
 @EnableFeignClients
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ShoppingPortalApplication {
 
     public static void main(String[] args) {
