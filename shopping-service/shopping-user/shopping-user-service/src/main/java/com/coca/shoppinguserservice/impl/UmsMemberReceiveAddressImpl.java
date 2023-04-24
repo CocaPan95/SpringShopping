e68c4c1e092b5a42610fbf6fb0a85b64656b23b3
2530470001 <2530470001@qq.com>
2023-04-24 13:39:07 +0800

    戴拿嵌入

--- shopping-service/shopping-user/shopping-user-service/src/main/java/com/coca/shoppinguserservice/impl/UmsMemberReceiveAddressImpl.java
+++ shopping-service/shopping-user/shopping-user-service/src/main/java/com/coca/shoppinguserservice/impl/UmsMemberReceiveAddressImpl.java
@@ -18,7 +18,7 @@ import java.util.List;
 public class UmsMemberReceiveAddressImpl implements UmsMemberReceiveAddressService {
     @Autowired
     private UmsMemberReceiveAddressMapper memberReceiveAddressMapper;
-    @DubboReference
+
     private UmsMemberService umsMemberService;
 
     public List<UmsMemberReceiveAddress> GetMemberReceiveAddressList(Long memberId){
