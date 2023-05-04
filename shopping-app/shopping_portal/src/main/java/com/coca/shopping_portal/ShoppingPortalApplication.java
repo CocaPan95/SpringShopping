fc95bb2aabedc44ece8957dc375c69f2d22e02c9
2530470001 <2530470001@qq.com>
2023-05-04 10:44:14 +0800

    引入seata成功，订单未测试，seata版本还有点问题

--- shopping-app/shopping_portal/src/main/java/com/coca/shopping_portal/ShoppingPortalApplication.java
+++ shopping-app/shopping_portal/src/main/java/com/coca/shopping_portal/ShoppingPortalApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDubbo
 @EnableFeignClients
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ShoppingPortalApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShoppingPortalApplication.class, args);
